6e30df5d50de46e49a7354e3feadb51686bee606
nham <hamann.nick@gmail.com>
2014-02-02 07:31:46 -0500

    checkpoint commit. probably doesnt compile

--- engine.rs
+++ engine.rs
@@ -1,13 +1,14 @@
 use std::fmt::{Default, Formatter};
 
-fn main() {
-    let mut p1 = Particle { m: 1.0, 
-                            pos: Vector2{x:-5.0, y:10.0},
-                            vel: Vector2{x:0.0, y:0.0} };
-    let mut p2 = Particle { m: 20.0, 
-                            pos: Vector2{x:5.0, y:10.0},
-                            vel: Vector2{x:0.0, y:0.0} };
+use util::Vector2;
 
+fn main() {
+    let mut p1 = Particle { m: 20.0, 
+                            pos: Vector2{x:-5.0, y:10.0} };
+    let mut p2 = Particle { m: 20.0, 
+                            pos: Vector2{x:5.0, y:10.0} };
+
+    let mut bod = Body::new(~[p1, p2], 0.0, Vector2 { x: 0.0, y: 0.0 }, 1.0);
 
     println!("P1: {}\nP2: {}", p1, p2);
     println!("--------");
@@ -15,38 +16,98 @@ fn main() {
     let dt = 0.01;
     let g = Vector2{x: 0.0, y: -9.8};
 
-    euler_step(&mut p1, 0.0, dt, |_: f64| -> Vector2 { g });
+ //   euler_step(&mut p1, 0.0, dt, |_: f64| -> Vector2 { g });
 
     println!("P1: {}\nP2: {}", p1, p2);
 
 }
 
-fn euler_step(p: &mut Particle, t: f64, dt: f64, force: |f64| -> Vector2) {
-    let acc = force(t).scale_copy(1.0/p.m);
+// each step we look at the forces on each point to find the total linear
+// acceleration and the total angular acceleration. the former determines
+// how the center of mass moves, the latter determines how the object rotates
+//
+// In order to calculate the angular acceleration, we need to calculate torque,
+// which means we need to know the distance from CM to each particle. the fast
+// way to do this is store this distance in the body. we shouldn't store the
+// complete position of each particle, but merely the relative vector
+fn euler_step(body: &mut Body, t: f64, dt: f64, force: ~[ForceFunc]) {
+    let mut tot_force = Vector{x: 0.0, y: 0.0};
+    let mut tot_torque = 0.0;
+    for (f, p) in force.iter().zip( body.particles.iter() ) {
+        tot_force.add( f(t) );
+        
+        let ang = body.ang - p.init_ang;
+        let unitx = Vector2 { x: 1.0, y: 0.0 };
+        tot_torque += f(t).dot( unitx.rotate(ang) );
+
+    }
+
+    // total linear acceleration
+    let lin_acc = tot_force(t).scale_copy(1.0/body.m);
     let delta_v = acc.scale_copy(dt);
+
+    body.linv.add( delta_v );
+    body.cm.add( body.linv.scale_copy(dt) );
+
+    let ang_acc = tot_torque.scale_copy(1.0 / body.mom);
+    /*
     p.vel.add( delta_v );
     p.pos.add( p.vel.scale_copy(dt) );
+    */
     
 }
 
+type Force = Vector2;
+type ForceFunc = |f64| -> Force;
 
+// We could try to just store
+//
+// does it really make sense to talk about the angle of the body? only seems to mean
+// something if we store the initial angle of each particle wrt the CoM. that way
+// we can store only the distances of each particle from CoM. we wouldnt have to
+// update the position vector at each step. how to calcuulate torque though? well,
+// we know what angle each position is at, as well as its distance. so we have just
+// find component of force perpendicular to that angle.
+//
+// the alternative is to not store body angle, but instead store the full position
+// vector of each particle. each step we update the position vectors as it rotates,
+// so we need to find the changle in angle and rotate each vector by that amount,
+// updating the Body.
 struct Body {
-    m: f64,
-    ps: ~[Particle],
+    particles: ~[RelParticle],
+    m: f64, // mass
+    mom: f64, // moment of inertia
+    ang: f64, // angle
+    angv: f64, // angular velocity
+    cm: Vector2, // center of mass
+    linv: Vector2, // linear velocity
 }
 
+struct RelParticle {
+    m: f64, // mass
+    r: f64, // distance from CoM
+    init_ang: f64, // initial angle
+}
+
+// Particles used to track their own velocities, but I removed them because generally
+// we will be dealing Particles as components of rigid bodies, where we do not need
+// to track the particle velocities individually.
+//
+// To recover the ability to model a single particle, we can create a Body with a
+// single particle in it. It's a bit of a hack, but does work.
+#[deriving(Clone)]
 struct Particle {
     m: f64,
     pos: Vector2,
-    vel: Vector2,
 }
 
 impl Default for Particle {
     fn fmt(p: &Particle, f: &mut Formatter) {
-        write!(f.buf, "(m: {}, pos: {}, vel: {})", p.m, p.pos, p.vel);
+        write!(f.buf, "(m: {}, pos: {})", p.m, p.pos);
     }
 }
 
+/*
 impl Particle {
     fn linmom(&self) -> Vector2 {
         self.vel.scale_copy( self.m )
@@ -57,80 +118,127 @@ impl Particle {
         
     }
 }
+*/
 
 
+// returns (Total Mass, Center of Mass vector)
+fn c_of_m(particles: ~[Particle]) -> (f64, Vector2) {
+    let mut cm = Vector2{ x: 0.0, y: 0.0};
+    let mut sum = 0.0;
+    for p in particles.iter() {
+        cm.add( p.pos.scale_copy(p.m) );
+        sum += p.m;
+    }
+
+    (sum, cm.scale_copy( 1.0 / sum ))
+}
+
+// computes the moment of inertia of a set of particles about some point
+fn moment_of_inertia(particles: ~[Particle], o: Vector2) -> f64 {
+    let mut sum = 0.0;
+    for p in particles.iter() {
+        sum += p.m * p.pos.sub_copy(o).normsq();
+    }
+    sum
+}
+
+
+// two ways we might like to construct a body:
+//   1) give a cloud of particles and the initial velocities of the body
+//   2) give a center of mass position and, for each particle of the body,
+//      its mass and distance/angle from CoM.
 impl Body {
-    fn new(ps: ~[Particle]) -> Body {
-        let mut sum = 0.0;
-        for p in ps.iter() {
-           sum += p.m;
+    // init_ang here serves to rotate the collection of particles (the given
+    // particle positions are taken to be angle = 0)
+    fn new(ps: ~[Particle], init_ang: f64, init_linv: Vector2, init_angv: f64) -> Body {
+        let (m, cm) = c_of_m(ps.clone());
+
+        Body { 
+            m: m,
+            cm: cm, 
+            mom: moment_of_inertia(ps, cm),
+            ang: init_ang, 
+            linv: init_linv,
+            angv: init_angv,
+            particles: ps,
+        }
+    }
+
+}
+
+
+
+mod util {
+    use std::fmt::{Default, Formatter};
+    use std::f64::{sqrt, cos, sin};
+
+    #[deriving(Clone)]
+    pub struct Vector2 {
+        x: f64,
+        y: f64,
+    }
+
+    impl Default for Vector2 {
+        fn fmt(v: &Vector2, f: &mut Formatter) {
+            write!(f.buf, "({}, {})", v.x, v.y);
+        }
+    }
+
+    impl Vector2 {
+        pub fn scale(&mut self, scalar: f64) {
+            self.x *= scalar;
+            self.y *= scalar;
         }
 
-        Body { m: sum, ps: ps }
-    }
-
-    fn linmom(&self) -> Vector2 {
-        self.c_of_m().scale_copy( self.m )
-    }
-
-    fn c_of_m(&self) -> Vector2 {
-        let mut cm = Vector2{ x: 0.0, y: 0.0};
-        for p in self.ps.iter() {
-            cm.add( p.linmom() );
+        pub fn scale_copy(&self, scalar: f64) -> Vector2 {
+            let mut new = self.clone();
+            new.x *= scalar;
+            new.y *= scalar;
+            new
         }
 
-        cm.scale_copy( 1.0 / self.m )
-    }
-}
+        pub fn add(&mut self, v: Vector2) {
+            self.x += v.x;
+            self.y += v.y;
+        }
 
+        pub fn add_copy(& self, v: Vector2) -> Vector2 {
+            let mut new = self.clone();
+            new.x += v.x;
+            new.y += v.y;
+            new
+        }
 
+        pub fn sub_copy(& self, v: Vector2) -> Vector2 {
+            self.add_copy( v.scale_copy(-1.0) )
+        }
 
-#[deriving(Clone)]
-struct Vector2 {
-    x: f64,
-    y: f64,
-}
+        pub fn dot(&self, v: Vector2) -> f64 {
+            (self.x * v.x) + (self.y * v.y)
+        }
 
-impl Default for Vector2 {
-    fn fmt(v: &Vector2, f: &mut Formatter) {
-        write!(f.buf, "({}, {})", v.x, v.y);
-    }
-}
+        pub fn normsq(&self) -> f64 {
+            self.dot(*self)
+        }
 
-impl Vector2 {
-    fn scale(&mut self, scalar: f64) {
-        self.x *= scalar;
-        self.y *= scalar;
-    }
+        pub fn norm(&self) -> f64 {
+            sqrt(self.normsq())
+        }
 
-    fn scale_copy(&self, scalar: f64) -> Vector2 {
-        let mut new = self.clone();
-        new.x *= scalar;
-        new.y *= scalar;
-        new
-    }
+        pub fn rotate(&self, ang: f64) {
+            let cosa = cos(ang);
+            let sina = sin(ang);
+            let x = self.x;
+            let y = self.y;
+            self.x = cosa * x + sina * y;
+            self.y = - sina * x + cosa * y;
+        }
 
-    fn add(&mut self, v: Vector2) {
-        self.x += v.x;
-        self.y += v.y;
-    }
-
-    fn add_copy(& self, v: Vector2) -> Vector2 {
-        let mut new = self.clone();
-        new.x += v.x;
-        new.y += v.y;
-        new
-    }
-
-    fn sub_copy(& self, v: Vector2) -> Vector2 {
-        self.add_copy( v.scale_copy(-1.0) )
-    }
-
-    fn dot(&self, v: Vector2) -> f64 {
-        (self.x * v.x) + (self.y * v.y)
-    }
-
-    fn norm(&self) -> f64 {
-        self.dot(*self)
+        pub fn rotate_copy(&self, ang: f64) -> Vector2 {
+            let cosa = cos(ang);
+            let sina = sin(ang);
+            Vector2 { x: cosa * self.x + sina * self.y,
+                      y: - sina * self.x + cosa * self.y }
+        }
     }
 }

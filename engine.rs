766e36032dd2307574539aeb6d58d4aab0d2ce9d
nham <hamann.nick@gmail.com>
2014-02-02 10:02:11 -0500

    move vector code to separate  module

--- engine.rs
+++ engine.rs
@@ -3,6 +3,8 @@ use std::f64::sqrt;
 
 use util::Vector2;
 
+mod util;
+
 fn main() {
     let mut p1 = Particle { m: 20.0, 
                             pos: Vector2{x:-5.0, y:10.0} };
@@ -161,86 +163,4 @@ impl Body {
         self.mom * self.angv
     }
 
-}
-
-
-
-mod util {
-    use std::fmt::{Default, Formatter};
-    use std::f64::{sqrt, cos, sin};
-
-    #[deriving(Clone)]
-    pub struct Vector2 {
-        x: f64,
-        y: f64,
-    }
-
-    impl Default for Vector2 {
-        fn fmt(v: &Vector2, f: &mut Formatter) {
-            write!(f.buf, "({}, {})", v.x, v.y);
-        }
-    }
-
-    impl Vector2 {
-        pub fn scale(&mut self, scalar: f64) {
-            self.x *= scalar;
-            self.y *= scalar;
-        }
-
-        pub fn scale_copy(&self, scalar: f64) -> Vector2 {
-            let mut new = self.clone();
-            new.x *= scalar;
-            new.y *= scalar;
-            new
-        }
-
-        pub fn add(&mut self, v: Vector2) {
-            self.x += v.x;
-            self.y += v.y;
-        }
-
-        pub fn add_copy(& self, v: Vector2) -> Vector2 {
-            let mut new = self.clone();
-            new.x += v.x;
-            new.y += v.y;
-            new
-        }
-
-        pub fn sub_copy(& self, v: Vector2) -> Vector2 {
-            self.add_copy( v.scale_copy(-1.0) )
-        }
-
-        pub fn dot(&self, v: Vector2) -> f64 {
-            (self.x * v.x) + (self.y * v.y)
-        }
-
-        pub fn normsq(&self) -> f64 {
-            self.dot(*self)
-        }
-
-        pub fn norm(&self) -> f64 {
-            sqrt(self.normsq())
-        }
-
-        pub fn rotate(&mut self, ang: f64) {
-            let cosa = cos(ang);
-            let sina = sin(ang);
-            let x = self.x;
-            let y = self.y;
-            self.x = cosa * x + sina * y;
-            self.y = - sina * x + cosa * y;
-        }
-
-        pub fn rotate_copy(&self, ang: f64) -> Vector2 {
-            let cosa = cos(ang);
-            let sina = sin(ang);
-            Vector2 { x: cosa * self.x + sina * self.y,
-                      y: - sina * self.x + cosa * self.y }
-        }
-
-        // calculates the angle of the vector wrt the x-axis [direction (1, 0)]
-        pub fn angx(&self) -> f64 {
-            self.dot( Vector2{ x:1.0, y: 1.0} ) / self.norm()
-        }
-    }
 }
---
+++ util.rs
@@ -0,0 +1,77 @@
+use std::fmt::{Default, Formatter};
+use std::f64::{sqrt, cos, sin};
+
+#[deriving(Clone)]
+pub struct Vector2 {
+    x: f64,
+    y: f64,
+}
+
+impl Default for Vector2 {
+    fn fmt(v: &Vector2, f: &mut Formatter) {
+        write!(f.buf, "({}, {})", v.x, v.y);
+    }
+}
+
+impl Vector2 {
+    pub fn scale(&mut self, scalar: f64) {
+        self.x *= scalar;
+        self.y *= scalar;
+    }
+
+    pub fn scale_copy(&self, scalar: f64) -> Vector2 {
+        let mut new = self.clone();
+        new.x *= scalar;
+        new.y *= scalar;
+        new
+    }
+
+    pub fn add(&mut self, v: Vector2) {
+        self.x += v.x;
+        self.y += v.y;
+    }
+
+    pub fn add_copy(& self, v: Vector2) -> Vector2 {
+        let mut new = self.clone();
+        new.x += v.x;
+        new.y += v.y;
+        new
+    }
+
+    pub fn sub_copy(& self, v: Vector2) -> Vector2 {
+        self.add_copy( v.scale_copy(-1.0) )
+    }
+
+    pub fn dot(&self, v: Vector2) -> f64 {
+        (self.x * v.x) + (self.y * v.y)
+    }
+
+    pub fn normsq(&self) -> f64 {
+        self.dot(*self)
+    }
+
+    pub fn norm(&self) -> f64 {
+        sqrt(self.normsq())
+    }
+
+    pub fn rotate(&mut self, ang: f64) {
+        let cosa = cos(ang);
+        let sina = sin(ang);
+        let x = self.x;
+        let y = self.y;
+        self.x = cosa * x + sina * y;
+        self.y = - sina * x + cosa * y;
+    }
+
+    pub fn rotate_copy(&self, ang: f64) -> Vector2 {
+        let cosa = cos(ang);
+        let sina = sin(ang);
+        Vector2 { x: cosa * self.x + sina * self.y,
+                  y: - sina * self.x + cosa * self.y }
+    }
+
+    // calculates the angle of the vector wrt the x-axis [direction (1, 0)]
+    pub fn angx(&self) -> f64 {
+        self.dot( Vector2{ x:1.0, y: 1.0} ) / self.norm()
+    }
+}
